00a1582b8648d74f63d6d75c4b20899f166c5c2a
Mohamed Es-saadani <essaadani80@gmail.com>
2022-03-04 09:33:46 +0100

    initial commit

---
+++ screens/reviewsDetails.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+
+export default function ReviewsDetails() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.titleText}>Reviews Details Screen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 24,
+  },
+  titleText: {
+    fontFamily: "nunito-bold",
+    fontSize: 18,
+  },
+});
